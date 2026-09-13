232c93f7a85d6e7ad9efb28882ec9f03f3b45e0a
momos00 <moremohit374@gmail.com>
2026-09-13 09:57:56 +0530

    Implement dummy HTTP server in main.py
    Added a simple HTTP server that responds with 'Bot is alive!'

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 import os
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import threading
+
+# Render ko portray karne ke liye dummy server
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is alive!")
+
+def run_dummy_server():
+    port = int(os.environ.get("PORT", 8080))
+    server = HTTPServer(('0.0.0.0', port), SimpleHandler)
+    server.serve_forever()
+
+# Background thread me dummy server start karo
+threading.Thread(target=run_dummy_server, daemon=True).start()
+import os
 import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
